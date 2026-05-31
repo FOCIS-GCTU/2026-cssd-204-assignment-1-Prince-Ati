cd44e64a9290428e04acdb6fb5670a41f2735bdb
Prince-Ati <2425401448@live.gctu.edu.gh>
2026-05-31 05:18:08 -0800

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Prince Kwasi Atiembo
+# STUDENT ID: 2425401448
+# Email: 2425401448@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Prince Kwasi Atiembo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+    # INITIALS
+    print()
+    print("...PAK")
+    print()
+    print("...PPPPPPPP.................AA.............KKK.......KK.....")
+    print("...PPPPPPPPPP..............AAAA............KKK.....KK.......")
+    print("...PPP......PPP...........AA..AA...........KKK....KK........")
+    print("...PPP.......PP..........AA....AA..........KKK..KK..........")
+    print("...PPPPPPPPPP...........AA......AA.........KKKKK............")
+    print("...PPPPPPPP............AAAAAAAAAAAA........KKK..KK..........")
+    print("...PPP.................AA........AA........KKK....KK........")
+    print("...PPP.................AA........AA........KKK.....KK.......")
+    print("...PPP............**...AA........AA...**...KKK......KK....**")
+    print("...PPP............**...AA........AA...**...KKK.......KK...**")
+    print()
 
 main()
